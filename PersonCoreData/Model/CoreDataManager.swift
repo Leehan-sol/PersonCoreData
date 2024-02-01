83508eb95c086064f28024911d61ee8a2b7ae419
이한솔 <>
2024-02-01 15:17:34 +0900

    feat: context 변수 생성, 로직 수정

--- PersonCoreData/Model/CoreDataManager.swift
+++ PersonCoreData/Model/CoreDataManager.swift
@@ -11,36 +11,38 @@ import CoreData
 struct CoreDataManager {
     static let shared = CoreDataManager()
     private let persistentContainer: NSPersistentContainer
+    private let context: NSManagedObjectContext
     
     // 1. PersistentContainer 생성
     private init() {
-         persistentContainer = NSPersistentContainer(name: "PersonCoreData")
-         persistentContainer.loadPersistentStores { (storeDescription, error) in
-             if let error = error as NSError? {
-                 fatalError("Unresolved error \(error), \(error.userInfo)")
-             }
-         }
-     }
+        persistentContainer = NSPersistentContainer(name: "PersonCoreData")
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        context = persistentContainer.viewContext
+    }
     
-    private func saveContext() {
+    func saveContext() {
         // 5. Context에 변화가 있을 시 내용 저장
-           if persistentContainer.viewContext.hasChanges {
-               do {
-                   try persistentContainer.viewContext.save()
-               } catch {
-                   let nserror = error as NSError
-                   fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-               }
-           }
-       }
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
     
     
     // Create
     func createPerson(name: String, age: String) {
         // 2. Context 기반으로 Entity 생성
-        let entity = NSEntityDescription.entity(forEntityName: "PersonCoreData", in: persistentContainer.viewContext)
+        let entity = NSEntityDescription.entity(forEntityName: "PersonCoreData", in: context)
         // 3. Entity를 기반으로 Object 생성
-        let person = NSManagedObject(entity: entity!, insertInto: persistentContainer.viewContext)
+        let person = NSManagedObject(entity: entity!, insertInto: context)
         // 4. Object에 값 setting
         person.setValue(name, forKey: "name")
         person.setValue(age, forKey: "age")
@@ -54,7 +56,7 @@ struct CoreDataManager {
         
         do {
             // 검색방법 1
-            let fetchPersonList = try persistentContainer.viewContext.fetch(PersonCoreData.fetchRequest())
+            let fetchPersonList = try context.fetch(PersonCoreData.fetchRequest())
             personList = fetchPersonList
         } catch {
             print("데이터를 가져오는 중 에러 발생: \(error)")
@@ -68,10 +70,10 @@ struct CoreDataManager {
     func updatePerson(name: String, age: String, index: Int) {
         // 검색방법 2
         // index를 가져와서 여기서 찾아서 데이터 업데이트
-        let fetchRequest = NSFetchRequest<PersonCoreData>(entityName: "PersonCoreData")
+        //        let fetchRequest: NSFetchRequest<PersonCoreData> = NSFetchRequest(entityName: "PersonCoreData")
         
         do {
-            let result = try persistentContainer.viewContext.fetch(fetchRequest)
+            let result = try context.fetch(PersonCoreData.fetchRequest())
             if index < result.count {
                 let personObject = result[index]
                 personObject.setValue(name, forKey: "name")
@@ -84,14 +86,16 @@ struct CoreDataManager {
             print("Failed to update person: \(error)")
         }
     }
-
+    
     
     // Delete
     func deletePerson(person: PersonCoreData) {
         // 인덱스까지 가진 person이라는 매개변수를 받아서 삭제만 실행
-        persistentContainer.viewContext.delete(person)
+        context.delete(person)
         saveContext()
     }
     
     
 }
+
+
